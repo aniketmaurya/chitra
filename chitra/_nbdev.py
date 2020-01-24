f9ab503292f467b064ee84269244c32aaf13a50d
Aniket Maurya <aniket.maurya@coviam.com>
2020-01-24 18:45:22 +0530

    CI update

--- chitra/_nbdev.py
+++ chitra/_nbdev.py
@@ -2,9 +2,10 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"read_image": "00_core.ipynb",
+         "resize_image": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://aniketmaurya.github.io/chitra/"
 
