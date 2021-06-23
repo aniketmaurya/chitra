57b306f261546203e77f93b473db89d5fff9f35b
Aniket Maurya <aniketmaurya@outlook.com>
2021-06-24 01:29:21 +0530

    update

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,6 +1,9 @@
+from tensorflow import keras
+
 from chitra.trainer import create_cnn
 
 
 def test_create_cnn():
     cnn = create_cnn('mobilenetv2', 1, weights=None)
-    assert cnn.trainable == True
+    assert cnn.trainable is True
+    assert isinstance(cnn, keras.models.Model)
