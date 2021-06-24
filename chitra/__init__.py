ed62789b650e09fe9bb0dc4f510d7f740da2a1c5
Aniket Maurya <aniketmaurya@outlook.com>
2021-06-24 22:21:01 +0530

    replace / with internal keyword (#70)
    * replace / with internal keyword
    
    * cache to home dir

--- chitra/__init__.py
+++ chitra/__init__.py
@@ -1,5 +1,5 @@
 """A Deep Learning Computer Vision Utility library"""
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0b1"
 
 from .image import Chitra
--- chitra/constants.py
+++ chitra/constants.py
@@ -1,3 +1,5 @@
+import os
+
 IMAGENET_LABEL_URL = 'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt'
 
 _TF = "tensorflow"
@@ -6,3 +8,7 @@ _TORCH = "torch"
 _TORCHVISION = "torchvision"
 _JAX = "jax"
 _FLAX = "flax"
+
+CHITRA_URL_SEP = '[-]'
+CACHE_DIR = os.environ.get('HOME', '.') + '/.chitra_cache'
+IMAGE_CACHE_DIR = f'{CACHE_DIR}/images/'
