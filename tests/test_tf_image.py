19b593116e9abc10c997430169992fb8ee73818c
Aniket Maurya <aniketmaurya@outlook.com>
2021-06-26 21:01:53 +0530

    add test :white_check_mark:

---
+++ tests/test_tf_image.py
@@ -0,0 +1,23 @@
+import os
+
+import tensorflow as tf
+
+from chitra import Chitra
+from chitra.tf_image import read_image
+from chitra.tf_image import resize_image
+
+chitra_banner = 'https://raw.githubusercontent.com/aniketmaurya/chitra/master/docs/assets/chitra_banner.png'
+image = Chitra(chitra_banner).image
+
+
+def test_read_image():
+    image.save('./temp.png')
+    tf_image = read_image('./temp.png')
+    assert 3 <= tf_image.shape[-1] <= 4
+    assert isinstance(tf_image, tf.Tensor)
+    os.remove('./temp.png')
+
+
+def test_resize_image():
+    assert resize_image(tf.random.normal((32, 32, 3)),
+                        (24, 24)).shape == (24, 24, 3)
