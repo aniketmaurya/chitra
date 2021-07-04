23636be64e28769789f28fb8b2ebaa0798b88ab5
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-04 22:38:11 +0530

    reformat

--- tests/test_model_server.py
+++ tests/test_model_server.py
@@ -2,16 +2,12 @@ import numpy as np
 from fastapi import FastAPI
 
 from chitra.serve import create_api
-from chitra.serve.model_server import _default_postprocess
+
+
+def dummy_model(x):
+    return np.asarray([1, 2])
 
 
 def test_create_app():
-    model = lambda x: x + 1
-    app = create_api(model)
+    app = create_api(dummy_model, "IMAGE-CLASSIFICATION")
     assert isinstance(app, FastAPI)
-
-
-def test__default_postprocess():
-    data = _default_postprocess(np.random.randn(5))
-    assert len(data) == 5
-    assert isinstance(data, list)
