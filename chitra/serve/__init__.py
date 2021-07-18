72195c8e4bad9fd76392ee4e947e556533d56b68
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-18 14:01:58 +0530

    refactor

--- chitra/serve/__init__.py
+++ chitra/serve/__init__.py
@@ -1,1 +1,2 @@
-from chitra.serve.model_server import API, create_api, get_available_api_types
+from chitra.serve.api import API, create_api
+from chitra.serve.model_server import get_available_api_types
