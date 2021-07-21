87418cd29f6946ae3a884e10374b94eb65bfb344
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-21 12:04:39 +0530

    fix

--- tests/data_processing/default/test_vision.py
+++ tests/data_processing/default/test_vision.py
@@ -4,14 +4,10 @@ from chitra.data_processing.default.vision import default_preprocess
 
 
 def test_default_preprocess():
-    image_arr = np.random.randn(16, 16, 3)
+    image_arr = np.random.randn(16, 16, 3).astype('uint8')
 
     result = default_preprocess(
         data=image_arr, image_shape=(32, 32), rescale=True, expand_dims=True
     )
     assert isinstance(result, np.ndarray)
-    assert result.shape == (32, 32)
-
-
-def test_default_postprocess():
-    pass
+    assert result.shape == (1, 32, 32, 3)
