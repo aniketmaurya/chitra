1f017f5bde0159677d89835f31a3ea536b183bfa
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-22 08:22:30 +0530

    version

--- chitra/__init__.py
+++ chitra/__init__.py
@@ -1,7 +1,7 @@
 """Deep Learning library for Model Building, Interpretability, Visualization,
 API Building & Deployment."""
 
-__version__ = "0.1.0rc0"
+__version__ = "0.1.0b4"
 __license__ = "Apache License 2.0"
 
 from chitra.image import Chitra
