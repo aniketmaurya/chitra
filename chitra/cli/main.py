1235281e2abe81273cc66744c6a439964349d241
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-30 08:05:53 +0530

    clean

--- chitra/cli/main.py
+++ chitra/cli/main.py
@@ -1,8 +1,18 @@
 import typer
 
-from chitra.cli import dockerize, examples
+from chitra import __version__
+from chitra.cli import dockerize
 
 app = typer.Typer(name="chitra CLI ✨")
 
 app.add_typer(dockerize.app, name="dockerizer")
-app.add_typer(examples.app, name="examples")
+
+
+@app.command()
+def hello():
+    typer.echo(f"Hey 👋! You're running chitra version={__version__} ✨")
+
+
+@app.command()
+def bye():
+    typer.echo("bye bye! 😊")
