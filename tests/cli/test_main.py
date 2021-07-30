41ea96cdcec25f8acb4603ad276de1d5aa752ad5
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-30 08:31:30 +0530

    add test :white_check_mark:

---
+++ tests/cli/test_main.py
@@ -0,0 +1,15 @@
+import typer
+from typer.testing import CliRunner
+
+from chitra import __version__
+from chitra.cli.main import version
+
+runner = CliRunner()
+
+
+def test_app():
+    app = typer.Typer()
+    app.command()(version)
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert f"You're running chitra version" in result.stdout
