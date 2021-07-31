8ca24a51b880bd220dabe995806061e2b0ab6481
Aniket Maurya <aniketmaurya@outlook.com>
2021-07-31 15:44:46 +0530

    add docker_utility.py

---
+++ chitra/utility/docker_utility.py
@@ -0,0 +1,39 @@
+import os
+from io import BytesIO
+from typing import Optional
+
+import docker
+
+from chitra.constants import DOCKER_BASE_URL
+
+client = docker.APIClient(base_url=os.environ.get("docker_base_url", DOCKER_BASE_URL))
+
+example = """
+# Shared Volume
+FROM busybox:buildroot-2014.02
+VOLUME /data
+CMD ["/bin/sh"]
+"""
+
+
+def is_connected() -> bool:
+    """
+    Check if Docker engine is running
+    Returns:
+        true if Docker engine is running
+    """
+    return client.ping()
+
+
+def build(
+    dockerfile: str, tag: Optional[str] = None, docker_kwargs: Optional[dict] = None
+):
+    if not docker_kwargs:
+        docker_kwargs = {}
+
+    fileobj = BytesIO(dockerfile.encode("utf-8"))
+    response = [
+        line
+        for line in client.build(fileobj=fileobj, rm=True, tag=tag, **docker_kwargs)
+    ]
+    return response
