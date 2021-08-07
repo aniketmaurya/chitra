99fc40030e053625f0b65d533e065bca367c0af2
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-07 15:11:20 +0530

    fix test

--- tests/cli/test_builder.py
+++ tests/cli/test_builder.py
@@ -1,9 +1,11 @@
 from unittest.mock import MagicMock, patch
 
+import pytest
 import typer
 from typer.testing import CliRunner
 
 from chitra.cli import builder
+from chitra.cli.builder import file_check
 
 runner = CliRunner()
 
@@ -13,7 +15,18 @@ runner = CliRunner()
 def test_app(mock_file_check, mock_subprocess):
     mock_file_check.return_value = True
     mock_subprocess.run = MagicMock()
+    mock_subprocess.run.return_value = MagicMock()
+    mock_subprocess.run.return_value.returncode = True
+
     app = typer.Typer()
     app.command()(builder.create)
-    result = runner.invoke(app, input="N\n")
+    result = runner.invoke(app, input="Y\n")
     assert result.exit_code == 0
+
+
+def test_file_check():
+    with pytest.raises(UserWarning):
+        file_check(["requirements.txt"])
+
+    with pytest.raises(UserWarning):
+        file_check([])
