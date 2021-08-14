6e8ec8a2996c7a5abfe68aae022e3a61d3b7da34
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-14 17:49:58 +0530

    fixes

--- tests/serve/cloud/test_chalice_server.py
+++ tests/serve/cloud/test_chalice_server.py
@@ -1,16 +1,35 @@
 import io
 
+import numpy as np
 import torch
 from chalice import Chalice
 from timm import create_model
 
+from chitra.core import load_imagenet_labels
+from chitra.image import Chitra
 from chitra.serve.cloud import ChaliceServer
 
-# MODEL_PATH = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/efficientnet_b0_ra-3dd342df.pth"
-MODEL_PATH = "examples/assets/model.pth"
+LABELS = load_imagenet_labels()
+
+MODEL_PATH = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/efficientnet_b0_ra-3dd342df.pth"
+
 url = (
     "https://raw.githubusercontent.com/aniketmaurya/chitra/master/docs/assets/logo.png"
 )
+
+
+def preprocess(content_raw_body) -> torch.Tensor:
+    image = Chitra(content_raw_body)
+    image.resize((256, 256))
+    x = image.numpy().astype(np.float32)
+    x = x / 255.0
+    x = torch.from_numpy(x)
+    x = x.permute(2, 0, 1).unsqueeze(0)
+    return x
+
+
+def postprocess(data: torch.Tensor) -> str:
+    return LABELS[data.argmax(1)]
 
 
 def model_loader(buffer: io.BytesIO) -> torch.nn.Module:
@@ -41,6 +60,8 @@ def test_predict():
         "image-classification",
         model_path=MODEL_PATH,
         model_loader=model_loader,
+        preprocess_fn=preprocess,
+        postprocess_fn=postprocess
     )
     server.app.current_request = Dummy
     assert isinstance(server.predict(), str)
