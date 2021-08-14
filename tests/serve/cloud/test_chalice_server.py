7115f5e789058f8e89b71f33c6f2cb1a3783c0df
Aniket Maurya <aniketmaurya@outlook.com>
2021-08-14 17:14:28 +0530

    fix tests

--- tests/serve/cloud/test_chalice_server.py
+++ tests/serve/cloud/test_chalice_server.py
@@ -6,8 +6,8 @@ from timm import create_model
 
 from chitra.serve.cloud import ChaliceServer
 
-# MODEL_PATH = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/efficientnet_b0_ra-3dd342df.pth"
-MODEL_PATH = "examples/assets/model.pth"
+MODEL_PATH = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/efficientnet_b0_ra-3dd342df.pth"
+# MODEL_PATH = "examples/assets/model.pth"
 
 
 def model_loader(buffer: io.BytesIO) -> torch.nn.Module:
