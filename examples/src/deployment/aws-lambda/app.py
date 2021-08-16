a34d287f428cee4584c13becb99e4c8d175698ac
Aniket Maurya <theaniketmaurya@gmail.com>
2021-08-16 09:36:12 +0530

    add help comments

--- examples/src/deployment/aws-lambda/app.py
+++ examples/src/deployment/aws-lambda/app.py
@@ -9,12 +9,15 @@ from chitra.core import load_imagenet_labels
 from chitra.image import Chitra
 from chitra.serve.cloud.aws_serverless import ChaliceServer
 
-MODEL_PATH = "../../../assets/model.pth"
+
+# This path can be anything from filesystem to cloud storage
+MODEL_PATH = "https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/efficientnet_b0_ra-3dd342df.pth"
 
 LABELS = load_imagenet_labels()
 logger.debug(f"labels={LABELS[:5]}...")
 
 
+# Preprocess the image
 def preprocess(content_raw_body) -> torch.Tensor:
     image = Chitra(content_raw_body)
     image.resize((256, 256))
@@ -25,12 +28,14 @@ def preprocess(content_raw_body) -> torch.Tensor:
     return x
 
 
+# Convert Imagenet Index to string label name
 def postprocess(data: torch.Tensor) -> str:
     logger.debug(f"predictions = {data}")
     result = LABELS[data.argmax(1)]
     return result
 
 
+# Loads model from io.BytesIO
 def model_loader(buffer: io.BytesIO) -> torch.nn.Module:
     model = create_model("efficientnet_b0", pretrained=False).eval()
     model.load_state_dict(torch.load(buffer))
